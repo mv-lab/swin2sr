301cca6f2bbcb570121c45b5c0cb2a67585bdd03
bogdan.vlad <bogdan.vlad@grabtaxi.com>
2023-04-27 23:20:47 +0300

    do not pad when dimensions of input image is already multiple of window size

--- predict.py
+++ predict.py
@@ -62,8 +62,8 @@ class Predictor(BasePredictor):
         with torch.no_grad():
             # pad input image to be a multiple of window_size
             _, _, h_old, w_old = img_lq.size()
-            h_pad = (h_old // window_size + 1) * window_size - h_old
-            w_pad = (w_old // window_size + 1) * window_size - w_old
+            h_pad = (window_size - h_old % window_size) % window_size
+            w_pad = (window_size - w_old % window_size) % window_size
             img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[
                 :, :, : h_old + h_pad, :
             ]
